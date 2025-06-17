e540a772e76e386e5eddf3b0d424461edc6aedf0
alfajri23 <123190088@student.upnyk.ac.id>
2025-06-17 21:01:19 +0700

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com','localhost'],
+        domains: ['images.unsplash.com','localhost','bkswko00okco8ck4ow0wkoo8.82.112.238.157.sslip.io'],
     },
 };
 
